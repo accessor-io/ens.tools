e361f72ac58e562a7c2541c4257f13e8c17b2c4c
acc <acc@accessor.io>
2026-02-26 01:26:25 -0800

    auto-fix cycle 7: unused_var: 7

--- src/components/registry/ContractRegistry.tsx
+++ src/components/registry/ContractRegistry.tsx
@@ -55,7 +55,7 @@ import {
 import { toast } from 'sonner';
 import { useWeb3 } from '../../lib/services';
 import { getAllTextRecords, formatAddress } from '../../lib/ens/ens-utils';
-import { granularPermissionService, parsePermissions } from '../../lib/services/granular-permission-service';
+import { granularPermissionService } from '../../lib/services/granular-permission-service'
 import { namehash } from '../../lib/ens/ens-helpers';
 
 interface Contract {
